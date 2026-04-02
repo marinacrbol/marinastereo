b58786cd97241c6128412719f177fbf0e9a04b4b
marinacrbol <marinacarbol@gmail.com>
2026-04-02 13:06:02 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,44 @@
 self.addEventListener('install', function(event) {
-console.log('Service Worker instalado');
+    console.log('Service Worker instalado');
+
+    event.waitUntil(
+        caches.open('radio-cache-v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/logo.jpg',
+                '/banner.jpg'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
-console.log('Service Worker activado');
+    console.log('Service Worker activado');
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== 'radio-cache-v1') {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-// No interceptamos nada, solo permitimos que la app funcione
+
+    // ❌ NO cachear el streaming
+    if (event.request.url.includes('/listen/')) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
